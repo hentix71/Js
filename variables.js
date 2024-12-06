2d88dab3193b53451de8782c2aeb340dba34631c
Hentix <iu3071pc@gmail.com>
2024-12-06 17:42:50 +0545

    numberConversion

---
+++ variables.js
@@ -0,0 +1,15 @@
+// for const 
+const accountId = 71
+let accountEmail = "iu33" // use let 
+var accountPass = "abc"  // dont use var
+
+console.table([accountId, accountEmail, accountPass])
+
+// conversion of data type
+let no = 71
+console.log(typeof(no));
+
+let stringNo = String(no)
+console.log(typeof(stringNo))
+console.log(stringNo)
+
